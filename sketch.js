c85bad1db424e03ff95230b7c0fcf8116e4fb417
vishaksathishkumar <74096175+vishaksathishkumar@users.noreply.github.com>
2021-06-01 14:14:52 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,1320 @@
+
+
+
+
+function setup() {
+	createCanvas(1500, 700);
+	rectMode(CENTER);
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+
+  
+
+if(keyCode === 3 ){
+		textSize(100);
+    fill("white");
+    text("3", 400, 400);
+  }
+
+  if(keyCode === 8 ){
+textSize(100);
+fill("white");
+text("8", 400, 400);
+}
+
+if(keyCode === 9 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("9", 400, 400);
+}
+
+if(keyCode === 12 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("12", 400, 400);
+}
+
+if(keyCode === 13 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("13", 400, 400);
+}
+
+if(keyCode === 16 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("16", 400, 400);
+}
+
+if(keyCode === 17 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("17", 400, 400);
+}
+
+if(keyCode === 18 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("18", 400, 400);
+}
+
+if(keyCode === 19 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("19", 400, 400);
+}
+
+if(keyCode === 20 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("20", 400, 400);
+}
+
+if(keyCode === 21 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("21", 400, 400);
+}
+
+if(keyCode === 27 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("27", 400, 400);
+}
+
+if(keyCode === 25 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("25", 400, 400);
+}
+
+if(keyCode === 28 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("28", 400, 400);
+}
+
+if(keyCode === 29 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("29", 400, 400);
+}
+
+if(keyCode === 32 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("32", 400, 400);
+}
+
+if(keyCode === 33 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("33", 400, 400);
+}
+
+if(keyCode === 34 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("34", 400, 400);
+}
+
+if(keyCode === 35){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("35", 400, 400);
+}
+
+if(keyCode === 36 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("36", 400, 400);
+}
+
+if(keyCode === 37 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("37", 400, 400);
+}
+
+if(keyCode === 38 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("38", 400, 400);
+}
+
+if(keyCode === 39 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("39", 400, 400);
+}
+
+if(keyCode === 40 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("40", 400, 400);
+}
+
+if(keyCode === 41 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("41", 400, 400);
+}
+
+if(keyCode === 42 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("42", 400, 400);
+}
+
+if(keyCode === 43 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("43", 400, 400);
+}
+
+if(keyCode === 44 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("44", 400, 400);
+}
+
+if(keyCode === 45 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("45", 400, 400);
+}
+
+if(keyCode === 46 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("46", 400, 400);
+}
+
+if(keyCode === 47 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("47", 400, 400);
+}
+
+if(keyCode === 48 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("48", 400, 400);
+}
+
+if(keyCode === 49 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("49", 400, 400);
+}
+
+if(keyCode === 50 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("50", 400, 400);
+}
+
+if(keyCode === 51  ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("51", 400, 400);
+}
+
+if(keyCode === 52 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("52", 400, 400);
+}
+
+if(keyCode === 53 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("53", 400, 400);
+}
+
+if(keyCode === 54 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("54", 400, 400);
+}
+
+if(keyCode === 55 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("55", 400, 400);
+}
+
+if(keyCode === 56 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("56", 400, 400);
+}
+
+if(keyCode === 57 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("57", 400, 400);
+}
+
+if(keyCode === 58 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("58", 400, 400);
+}
+
+if(keyCode === 59 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("59", 400, 400);
+}
+
+if(keyCode === 60 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("60", 400, 400);
+}
+
+if(keyCode === 61 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("61", 400, 400);
+}
+
+if(keyCode === 62 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("62", 400, 400);
+}
+
+if(keyCode === 63 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("63", 400, 400);
+}
+
+if(keyCode === 64 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("64", 400, 400);
+}
+
+if(keyCode === 65 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("65", 400, 400);
+}
+
+if(keyCode === 66 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("66", 400, 400);
+}
+
+if(keyCode === 67 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("67", 400, 400);
+}
+
+if(keyCode === 68 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("68", 400, 400);
+}
+
+if(keyCode === 69 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("69", 400, 400);
+}
+
+if(keyCode === 70 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("70", 400, 400);
+}
+
+if(keyCode === 71 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("71", 400, 400);
+}
+
+if(keyCode === 72 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("72", 400, 400);
+}
+
+if(keyCode === 73 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("73", 400, 400);
+}
+
+if(keyCode === 74){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("74", 400, 400);
+}
+
+if(keyCode === 75 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("75", 400, 400);
+}
+
+if(keyCode === 76 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("76", 400, 400);
+}
+
+if(keyCode === 77 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("77", 400, 400);
+}
+
+if(keyCode === 78 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("78", 400, 400);
+}
+
+if(keyCode === 79 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("79", 400, 400);
+}
+
+if(keyCode === 80 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("80", 400, 400);
+}
+
+if(keyCode === 81 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("81", 400, 400);
+}
+
+if(keyCode === 82 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("82", 400, 400);
+}
+
+if(keyCode === 83 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("83", 400, 400);
+}
+
+if(keyCode === 84 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("84", 400, 400);
+}
+
+if(keyCode === 85 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("85", 400, 400);
+}
+
+if(keyCode === 86 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("86", 400, 400);
+}
+
+if(keyCode === 87 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("87", 400, 400);
+}
+
+if(keyCode === 88 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("88", 400, 400);
+}
+
+if(keyCode === 89 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("89", 400, 400);
+}
+
+if(keyCode === 90 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("90", 400, 400);
+}
+
+if(keyCode === 91 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("91", 400, 400);
+}
+	
+if(keyCode === 92 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("92", 400, 400);
+}  
+	
+if(keyCode === 93 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("93", 400, 400);
+}
+
+if(keyCode === 94 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("94", 400, 400);
+}
+
+if(keyCode === 95 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("95", 400, 400);
+}
+
+if(keyCode === 96 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("96", 400, 400);
+}
+
+if(keyCode === 97 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("97", 400, 400);
+}
+
+if(keyCode === 98 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("98", 400, 400);
+}
+
+if(keyCode === 99 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("99", 400, 400);
+}
+
+if(keyCode === 100 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("100", 400, 400);
+}
+
+if(keyCode === 101 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("101", 400, 400);
+}
+
+if(keyCode === 102 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("102", 400, 400);
+}
+
+if(keyCode === 103 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("103", 400, 400);
+}
+
+if(keyCode === 104 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("104", 400, 400);
+}
+
+if(keyCode === 105 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("105", 400, 400);
+}
+
+if(keyCode === 106 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("106", 400, 400);
+}
+
+if(keyCode === 107 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("107", 400, 400);
+}
+
+if(keyCode === 108 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("108", 400, 400);
+}
+
+if(keyCode === 108 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("108", 400, 400);
+}
+
+if(keyCode === 109 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("109", 400, 400);
+}
+
+if(keyCode === 110 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("110", 400, 400);
+}
+
+if(keyCode === 111 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("111", 400, 400);
+}
+
+if(keyCode === 112 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("112", 400, 400);
+}
+
+if(keyCode === 113 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("113", 400, 400);
+}
+
+if(keyCode === 114 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("114", 400, 400);
+}
+
+if(keyCode === 115 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("115", 400, 400);
+}
+
+if(keyCode === 116 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("116", 400, 400);
+}
+
+if(keyCode === 117 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("117", 400, 400);
+}
+
+if(keyCode === 118 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("118", 400, 400);
+}
+
+if(keyCode === 119 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("119", 400, 400);
+}
+
+if(keyCode === 120 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("120", 400, 400);
+}
+
+if(keyCode === 121 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("121", 400, 400);
+}
+
+if(keyCode === 122 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("122", 400, 400);
+}
+
+if(keyCode === 123 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("123", 400, 400);
+}
+
+if(keyCode === 124 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("124", 400, 400);
+}
+
+if(keyCode === 125 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("125", 400, 400);
+}
+
+if(keyCode === 126 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("126", 400, 400);
+}
+
+if(keyCode === 127 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("127", 400, 400);
+}
+
+if(keyCode === 128 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("128", 400, 400);
+}
+
+if(keyCode === 129 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("129", 400, 400);
+}
+
+if(keyCode === 130 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("130", 400, 400);
+}
+
+if(keyCode === 131 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("131", 400, 400);
+}
+
+if(keyCode === 132 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("132", 400, 400);
+}
+
+if(keyCode === 133 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("133", 400, 400);
+}
+
+if(keyCode === 134 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("134", 400, 400);
+}
+
+if(keyCode === 135 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("135", 400, 400);
+}
+
+if(keyCode === 136 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("136", 400, 400);
+}
+
+if(keyCode === 137 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("137", 400, 400);
+}
+
+if(keyCode === 138 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("138", 400, 400);
+}
+
+if(keyCode === 139 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("139", 400, 400);
+}
+
+if(keyCode === 140 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("140", 400, 400);
+}
+
+if(keyCode === 141 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("141", 400, 400);
+}
+
+if(keyCode === 142 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("142", 400, 400);
+}
+
+if(keyCode === 143 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("143", 400, 400);
+}
+
+if(keyCode === 144 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("144", 400, 400);
+}
+
+if(keyCode === 145 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("145", 400, 400);
+}
+
+if(keyCode === 151 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("151", 400, 400);
+}
+
+if(keyCode === 160 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("160", 400, 400);
+}
+
+if(keyCode === 161 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("161", 400, 400);
+}
+
+if(keyCode === 161 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("161", 400, 400);
+}
+
+if(keyCode === 162 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("162", 400, 400);
+}
+
+if(keyCode === 163 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("163", 400, 400);
+}
+
+if(keyCode === 164 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("164", 400, 400);
+}
+
+if(keyCode === 165 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("165", 400, 400);
+}
+
+if(keyCode === 166 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("166", 400, 400);
+}
+
+if(keyCode === 167 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("167", 400, 400);
+}
+
+if(keyCode === 168 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("120", 400, 400);
+}
+
+if(keyCode === 169 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("169", 400, 400);
+}
+
+if(keyCode === 170 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("170", 400, 400);
+}
+
+if(keyCode === 171 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("171", 400, 400);
+}
+
+if(keyCode === 172 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("172", 400, 400);
+}
+
+if(keyCode === 173 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("173", 400, 400);
+}
+
+if(keyCode === 174 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("174", 400, 400);
+}
+
+if(keyCode === 175 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("175", 400, 400);
+}
+
+if(keyCode === 176 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("176", 400, 400);
+}
+
+if(keyCode === 177 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("177", 400, 400);
+}
+
+if(keyCode === 178 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("178", 400, 400);
+}
+
+if(keyCode === 179 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("179", 400, 400);
+}
+
+if(keyCode === 180 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("180", 400, 400);
+}
+
+if(keyCode === 181 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("181", 400, 400);
+}
+
+if(keyCode === 182 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("182", 400, 400);
+}
+
+if(keyCode === 183 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("183", 400, 400);
+}
+
+if(keyCode === 184 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("184", 400, 400);
+}
+
+if(keyCode === 185 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("185", 400, 400);
+}
+
+if(keyCode === 186 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("186", 400, 400);
+}
+
+if(keyCode === 187 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("187", 400, 400);
+}
+
+if(keyCode === 188 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("188", 400, 400);
+}
+
+if(keyCode === 189 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("189", 400, 400);
+}
+
+if(keyCode === 190 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("190", 400, 400);
+}
+
+if(keyCode === 191 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("191", 400, 400);
+}
+
+if(keyCode === 192 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("192", 400, 400);
+}
+
+if(keyCode === 193 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("193", 400, 400);
+}
+
+if(keyCode === 194 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("194", 400, 400);
+}
+
+if(keyCode === 219 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("219", 400, 400);
+}
+
+if(keyCode === 220 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("220", 400, 400);
+}
+
+if(keyCode === 221 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("221", 400, 400);
+}
+
+if(keyCode === 222 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("222", 400, 400);
+}
+
+if(keyCode === 223 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("223", 400, 400);
+}
+
+if(keyCode === 224 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("224", 400, 400);
+}
+
+if(keyCode === 225 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("225", 400, 400);
+}
+
+if(keyCode === 226 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("226", 400, 400);
+}
+
+if(keyCode === 230 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("230", 400, 400);
+}
+
+if(keyCode === 231 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("231", 400, 400);
+}
+
+if(keyCode === 233 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("233", 400, 400);
+}
+
+if(keyCode === 234 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("234", 400, 400);
+}
+
+if(keyCode === 235 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("235", 400, 400);
+}
+
+if(keyCode === 240 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("240", 400, 400);
+}
+
+if(keyCode === 242 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("242", 400, 400);
+}
+
+if(keyCode === 243 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("243", 400, 400);
+}
+
+if(keyCode === 244 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("244", 400, 400);
+}
+
+if(keyCode === 251 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("251", 400, 400);
+}
+
+if(keyCode === 255 ){
+	textSize(100);
+textAlign(CENTER);
+fill("white");
+text("255", 400, 400);
+}
+
+drawSprites();
+}
+
+ 
